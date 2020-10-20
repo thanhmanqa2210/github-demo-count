354a7aedc09253b9aed2b01a8b9862cfeeb53ee2
ThanhManLab <Thanhmantm1999@gmail.com>
2020-10-20 14:18:50 +0700

    C4

--- demo.dart
+++ demo.dart
@@ -1,5 +1,6 @@
 void main() {
   print("chào mọi người hôm nay là thứ bảy");
   print("  Code không ngừng nghỉ nha thằng chó  kia  ");
-  print(" Như quần què");
+  print("đã sửa đổi lần 1 ");
+  print(" Bây giờ đúng chưa ");
 }
